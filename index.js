6c28e9995a06fe1d62daf823be318fce9689c9c4
jobhi <jobishlal761@gmail.com>
2025-03-16 18:24:37 +0530

    jobhi

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+   
+
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send("helo")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is runing");
+    
+})
