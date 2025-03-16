f2b39dcd6ee0acd46f2ad16c02cc6397178d0ea0
jobhi <jobishlal761@gmail.com>
2025-03-16 18:42:45 +0530

    hellO

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send("helo")
 })
+console.log("dsnjknjkfsdkjvjksfdbvjkdbvjkbdsvjkbjksbkjfdnv");
 
 app.listen(3000,()=>{
     console.log("Server is runing");
