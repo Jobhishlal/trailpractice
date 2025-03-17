77e5e6fe3dc97a0056f8503d1a9bfb5f39afb6fb
jobhi <jobishlal761@gmail.com>
2025-03-17 12:32:18 +0530

    undo

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send("helo")
 })
-console.log("dsnjknjkfsdkjvjksfdbvjkdbvjkbdsvjkbjksbkjfdnv");
+console.log("dsnjknjkfsvjkbjksbkjfdnv");
+console.log("reset");
+
 
 app.listen(3000,()=>{
     console.log("Server is runing");
